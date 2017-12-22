7b40ebd21d39419db39ffd0a94a3375f2a637ce0
Rahul Bethi <rahulbethi@hotmail.com>
2017-12-22 02:51:44 -0800

    Created movies class and added movies list

---
+++ main.py
@@ -0,0 +1,59 @@
+import movies
+
+avatar = movies.Movies("Avatar",
+                       "Avatar",
+                       "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp",
+                       "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+casino_royale = movies.Movies("Casino Royale",
+                              "James Bond",
+                              "http://www.gstatic.com/tv/thumb/movieposters/159167/p159167_p_v8_aa.jpg",
+                              "https://www.youtube.com/watch?v=36mnx8dBbGE")
+
+cars = movies.Movies("Cars",
+                     "Cars",
+                     "http://www.gstatic.com/tv/thumb/movieposters/159400/p159400_p_v8_aa.jpg",
+                     "https://www.youtube.com/watch?v=SbXIj2T-_uk")
+
+hurt_locker = movies.Movies("The Hurt Locker",
+                            "The Hurt Locker",
+                            "http://www.gstatic.com/tv/thumb/movieposters/197175/p197175_p_v8_ah.jpg",
+                            "https://www.youtube.com/watch?v=AIbFvqFYRT4")
+
+quantum_of_solace = movies.Movies("Quantum of Solace",
+                                  "James Bond",
+                                  "http://www.gstatic.com/tv/thumb/movieposters/170973/p170973_p_v8_ah.jpg",
+                                  "https://www.youtube.com/watch?v=f6acw690AqQ")
+
+blood_diamond = movies.Movies("Blood Diamond",
+                              "Blood Diamond",
+                              "http://www.gstatic.com/tv/thumb/movieposters/162986/p162986_p_v8_ab.jpg",
+                              "https://www.youtube.com/watch?v=yknIZsvQjG4")
+
+terminator_2 = movies.Movies("Terminator 2: Judgement Day",
+                             "Terminator",
+                             "http://t1.gstatic.com/images?q=tbn:ANd9GcS5J6Ay6y1UT7WAI4U7Zm2KDYITrvfOI3vmaCNdGhx_0jmWiI1d",
+                             "https://www.youtube.com/watch?v=VVZQ39i5G1s")
+
+top_gun = movies.Movies("Top Gun",
+                        "Top Gun",
+                        "http://t2.gstatic.com/images?q=tbn:ANd9GcSVCaXEcuVO3eMn2X0c0_TGdISkfQMfyy1lRkTZ88sfIIj-g-5d",
+                        "https://www.youtube.com/watch?v=VN8ze3S0Uj8")
+
+god_father = movies.Movies("God Father",
+                           "God Father",
+                           "http://3.bp.blogspot.com/-rj68Bw1uQJ4/TzQ9P3yaSiI/AAAAAAAAB0E/nj1kVB09M8A/s1600/godfather_ver1.jpg",
+                           "https://www.youtube.com/watch?v=5DO-nDW43Ik")
+
+fast_and_furious = movies.Movies("The Fast and the Furious",
+                           "Fast and Furious",
+                           "https://vignette.wikia.nocookie.net/fastandfurious/images/0/04/The_Fast_and_the_Furious_%28DVD_Cover%29.jpeg/revision/latest?cb=20150501043627",
+                           "https://www.youtube.com/watch?v=ZsJz2TJAPjw")
+
+titanic = movies.Movies("Titanic",
+                        "Titanic",
+                        "https://i.ytimg.com/vi/jIhicnTgArM/movieposter.jpg",
+                        "https://www.youtube.com/watch?v=zCy5WQ9S4c0")
+
+movies_list = [avatar, casino_royale, cars, hurt_locker, quantum_of_solace, blood_diamond,
+               terminator_2, top_gun, god_father, fast_and_furious, titanic]
